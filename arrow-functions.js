54fef2828cf0e3946710d1c6a5c8ee261bc77e8f
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2025-01-18 02:46:04 +0000

    Completed exercises

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbersVanilla(a, b) {
+    // Code Block
+    return a + b;
+}
+let sumVanilla = addTwoNumbersVanilla(3, 5);
+console.log(sumVanilla);
 
 // Arrow Function With Parameters
+const addTwoNumbersE6 = (a, b) => {
+    // Code Block
+    return a + b;
+}
+let sumE6 = addTwoNumbersE6(3, 5);
+console.log(sumE6);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbersSingleLine = (a, b) => a + b;
+
+let sumSingleLine = addTwoNumbersSingleLine(5, 3);
+console.log(sumSingleLine);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello old friend!');
+const sayHello = () => console.log('Hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultiLineString = () => (
+    `<p>
+        This is a multiline String!
+     <p>`
+);
+     console.log(returnMultiLineString());
